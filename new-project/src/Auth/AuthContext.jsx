fac9cf8d79d04b3b9e65425de4ddbc45109dbab1
pravingurav <pravingurav18@gmail.com>
2025-08-05 12:15:16 +0530

    changes

--- new-project/src/Auth/AuthContext.jsx
+++ new-project/src/Auth/AuthContext.jsx
@@ -4,16 +4,19 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
+
   useEffect(() => {
     const savedToken = localStorage.getItem('authToken');
     if (savedToken) {
       setToken(savedToken);
     }
   }, []);
+
   const login = (newToken) => {
     localStorage.setItem('authToken', newToken);
     setToken(newToken);
   };
+
   const logout = () => {
     localStorage.removeItem('authToken');
     setToken(null);
