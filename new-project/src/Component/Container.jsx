31f5bb1e83ac23ce9978a7b98e44cd690406ff75
pravingurav <pravingurav18@gmail.com>
2025-08-06 16:11:17 +0530

    changes

--- new-project/src/Component/Container.jsx
+++ new-project/src/Component/Container.jsx
@@ -11,7 +11,7 @@ const Container = () => {
       <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
       <div className="flex flex-col flex-1">
         <Header onMenuClick={() => setSidebarOpen(true)} />
-        <main className="flex-1 bg-gray-50 p-6 lg:ml-64">
+        <main className="flex-1 bg-gray-50  lg:ml-64">
   <Outlet />
 </main>
 
